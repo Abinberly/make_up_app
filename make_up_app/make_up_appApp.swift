9080c4830d780e8afe5f286e549c29f153ca1874
Abin Berly <abinberly12@gmail.com>
2024-04-01 11:40:17 +0530

    Initial Commit

---
+++ make_up_app/make_up_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  make_up_appApp.swift
+//  make_up_app
+//
+//  Created by Abin Berly on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct make_up_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
